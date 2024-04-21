c69b1443d78cc8cfc7a3612f05ed46231099b259
Rubbershredder <145557629+Rubbershredder@users.noreply.github.com>
2024-04-21 19:10:58 +0530

    Updated the navbar the video the collegebg

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
@@ -16,47 +16,74 @@ const Footer = () => {
           {/* Enquiry */}
           <div className="w-full md:w-auto mb-4 md:mb-0">
             <h3 className="text-lg font-bold mb-2">Enquiry</h3>
-            <p>Email: <a href="mailto:enquiry@svcengg.edu.in">enquiry@svcengg.edu.in</a></p>
+            <p>
+              Email:{" "}
+              <a href="mailto:enquiry@svcengg.edu.in">enquiry@svcengg.edu.in</a>
+            </p>
             <p>Phone: +919731226262</p>
             <p>Phone: +919740202345</p>
           </div>
           {/* Admissions */}
           <div className="w-full md:w-auto mb-4 md:mb-0">
             <h3 className="text-lg font-bold mb-2">Admissions</h3>
-            <p>Email: <a href="mailto:admissions@svcengg.edu.in">admissions@svcengg.edu.in</a></p>
+            <p>
+              Email:{" "}
+              <a href="mailto:admissions@svcengg.edu.in">
+                admissions@svcengg.edu.in
+              </a>
+            </p>
             <p>Phone: +91 9916775988</p>
           </div>
           {/* Examination */}
           <div className="w-full md:w-auto mb-4 md:mb-0">
             <h3 className="text-lg font-bold mb-2">Examination</h3>
-            <p>Email: <a href="mailto:exams@svcengg.edu.in">exams@svcengg.edu.in</a></p>
+            <p>
+              Email:{" "}
+              <a href="mailto:exams@svcengg.edu.in">exams@svcengg.edu.in</a>
+            </p>
             <p>Phone: +91 9901335353</p>
           </div>
         </div>
-        
+
         {/* Follow Us On */}
-        <div className="flex justify-center items-center text-lg font-bold mb-2">Follow Us On</div>
-        
-{/* Social icons */}
-<div className="flex justify-center items-center">
-  {/* Social icons */}
-  <div className="flex space-x-4">
-  <a href="https://www.facebook.com/svcebangalore/" target="_blank" rel="noopener noreferrer">
-    <img src="src/Components/Footer/facebook.png" alt="Facebook" />
-  </a>
-  <a href="https://www.instagram.com/svce_bengaluru/" target="_blank" rel="noopener noreferrer">
-    <img src="src/Components/Footer/Insta.png" alt="Instagram" />
-  </a>
-  <a href="https://x.com/SvceBengaluru?t=_W2I9zhdHZgV8mgTBev73A&s=09" target="_blank" rel="noopener noreferrer">
-    <img src="src/Components/Footer/twitter.png" alt="Twitter" />
-  </a>
-  <a href="https://www.youtube.com/@svcebangalore2290" target="_blank" rel="noopener noreferrer">
-    <img src="src/Components/Footer/youtube.png" alt="Youtube" />
-  </a>
-</div>
+        <div className="flex justify-center items-center text-lg font-bold mb-2">
+          Follow Us On
+        </div>
 
-</div>
-
+        {/* Social icons */}
+        <div className="flex justify-center items-center">
+          {/* Social icons */}
+          <div className="flex space-x-4">
+            <a
+              href="https://www.facebook.com/svcebangalore/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="src/Components/Footer/facebook.png" alt="Facebook" />
+            </a>
+            <a
+              href="https://www.instagram.com/svce_bengaluru/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="src/Components/Footer/Insta.png" alt="Instagram" />
+            </a>
+            <a
+              href="https://x.com/SvceBengaluru?t=_W2I9zhdHZgV8mgTBev73A&s=09"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="src/Components/Footer/twitter.png" alt="Twitter" />
+            </a>
+            <a
+              href="https://www.youtube.com/@svcebangalore2290"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="src/Components/Footer/youtube.png" alt="Youtube" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
